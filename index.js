633da3011ffab15fd9e8f68352e7c3cafbf30eb3
ms1835 <mayanksingh5399@gmail.com>
2021-07-27 01:20:42 +0530

    Refactoring

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
 const express = require('express')
 const app = express()
-const path = require('path')
+
 
 
 app.use(express.json())
-// app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.get('/api',(req,res)=>{
     res.json({message:"Hello from server"})
 })
 
-app.get('/',(req,res)=>{
-    // res.sendFile(__dirname, '../client/public/index.html')
-    res.json({value:"kuch nhi mil rhi"})
-})
 
-app.get('*',(req,res)=>{
-    res.send("Welcome to this page.")
-})
 
 app.post('/post',(req,res)=>{
     console.log("Connected to server")
     res.redirect('/')
+})
+
+app.get('*',(req,res)=>{
+    res.send("Undefined reference.")
 })
 
 const PORT = 3001
